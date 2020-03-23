a4589eff142a51f01b975119b3a1b9765be5186e
vaso <v.chigaev@gmail.com>
2020-03-24 01:31:19 +0300

    old

--- src/com/timbuchalka/Largestprime.java
+++ src/com/timbuchalka/Largestprime.java
@@ -3,21 +3,25 @@ package com.timbuchalka;
 public class Largestprime {
 
     public static void main(String[] args) {
-        System.out.println(getLargestPrime(8));
+        System.out.println(getLargestPrime(217));
     }
 
     public static int getLargestPrime(int number){
         int largestprime = -1;
-
+        if (number <= 1) return largestprime;
+        boolean isPrime;
         for (int i=1; i<=number; i++){
-            boolean isPrime = true;
+            isPrime = true;
             for (int y=2; y<i; y++){
                 if (i % y == 0) {
                     isPrime = false;
                     break;
                 }
             }
-            if (isPrime) largestprime = i;
+            if (isPrime) {
+                if (number % i == 0)
+                    largestprime = i;
+            }
         }
         return largestprime;
     }
