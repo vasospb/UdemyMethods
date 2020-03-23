a50cbd224a704137e792c986ad46695f5b3f6131
Vasily <v.chigaev@gmail.com>
2020-03-23 23:57:06 +0300

    LargePrime

---
+++ src/com/timbuchalka/Largestprime.java
@@ -0,0 +1,24 @@
+package com.timbuchalka;
+
+public class Largestprime {
+
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(7));
+    }
+
+    public static int getLargestPrime(int number){
+        int largestprime = -1;
+
+        for (int i=1; i<=number; i++){
+            boolean isPrime = true;
+            for (int y=2; y<i; y++){
+                if (i % y == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) largestprime = i;
+        }
+        return largestprime;
+    }
+}
